9626874b6528ab0720303ccf2e37d009145093b7
JacquelineMG <j.gilchrist@outlook.com>
2024-11-06 09:33:21 -0800

    work through reading

--- workpad.js
+++ workpad.js
@@ -1,14 +1,41 @@
-const a = [1,2];
-const b = a;
+// const map = [1,2,3].map((x) => {
+//   const y = x + 1;
+//   return x * y;
+// });
 
-console.log(a);
-console.log(b);
+// console.log(map);
 
-{ let c = 1;
-  const d = 2;
-  var e = 3;
-}
+// const arrLike = {0: "aaa", 1: "bbb", 2: "ccc"};
 
-//console.log(c);
-//console.log(d);
-console.log(e);
+// arrLike.length = Object.keys(arrLike).length;
+// console.log(arrLike);
+
+// const arr = Array.from(arrLike);
+
+// console.log(arr);
+
+
+// const a = [1,2];
+// const b = a;
+
+// console.log(a);
+// console.log(b);
+
+// { let c = 1;
+//   const d = 2;
+//   var e = 3;
+// }
+
+// console.log(c);
+// console.log(d);
+// console.log(e);
+
+// const items = [1,2,3,4,5];
+
+// const itemsCopy1 = [];
+
+// for (let i = 0; i < items.length; i += 1) {
+//   itemsCopy1[i] = items[i];
+// }
+
+// const itemsCopy2 = [...items];
