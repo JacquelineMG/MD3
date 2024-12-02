5728886b7c387ca278cade7a664a487925538cb3
JacquelineMG <j.gilchrist@outlook.com>
2024-12-02 14:44:10 -0800

    work on server exercise

--- WK6/express/server.js
+++ WK6/express/server.js
@@ -2,6 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get("/path", (req, res) => {
+  res.send("Keep on heading down that path...");
+});
+
+
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
