d0e71bb3bb527245d0904265f9bbebcf328e2538
JacquelineMG <j.gilchrist@outlook.com>
2024-12-27 09:44:15 -0800

    add view and routes for url submissions

--- WK6/tinyApp/express_server.js
+++ WK6/tinyApp/express_server.js
@@ -5,10 +5,23 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
+};
+
+const generateRandomString = () => {
+  const alphanums = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  let id = "";
+
+  while (id.length < 6) {
+    const random = Math.floor(Math.random() * (35 - 0 + 1) + 0);
+    id += (alphanums[random]);
+  }
+  return id;
 };
 
 app.get("/", (req, res) => {
@@ -28,10 +41,20 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new.ejs");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("OK");
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
